485eae36eb4decfb64d49b65ff8a72c8f8094dcf
drsmolina <daphnerosemolina@gmail.com>
2025-08-13 18:04:29 +0800

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+
+const basePath = "/REPO_NAME/";
+
+export default defineConfig({
+  plugins: [react()],
+  base: basePath
+});
